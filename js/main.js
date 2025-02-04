ad27c175309f54fb986bcfda9c4c103281eefc98
Dmitry <dmitronus2015@gmail.com>
2025-02-04 21:59:14 +0200

    add mobile menu js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
+const headerBtn = document.querySelector(".header__btn");
+const headerMenu = document.querySelector(".menu__list");
 
-});
+headerBtn.addEventListener("click", onHeaderBtnClick);
+
+function onHeaderBtnClick() {
+  headerMenu.classList.toggle("menu__list--active");
+}
