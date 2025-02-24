b785db5157edfb1955835a90d335f160fbc81671
Dmitry <dmitronus2015@gmail.com>
2025-02-24 19:05:12 +0200

    add heroes js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+$(".heroes__list").slick({
+  nextArrow: '<button type="button" class="slick-next"><img src="./img/arrow-right.svg" alt=""></button>',
+  prevArrow: '<button type="button" class="slick-prev"><img src="./img/arrow-left.svg" alt=""></button>',
+});
+
 const headerBtn = document.querySelector(".header__btn");
 const headerMenu = document.querySelector(".menu__list");
 
